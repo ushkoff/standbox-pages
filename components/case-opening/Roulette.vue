6b237ed8336cafec7c04c71d17c98447c3702b42
Ushkoff <52123759+Ushkoff@users.noreply.github.com>
2020-02-26 00:26:55 +0200

    Case opening page with components (no logic)

---
+++ components/case-opening/Roulette.vue
@@ -0,0 +1,148 @@
+<template>
+<div class="roulette">
+  <div class="roulette__case-title">
+    <div class="circles">
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+    <span>Ножевой</span>
+    <div class="circles">
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+  </div>
+  <span class="roulette__case-type">Кейс</span>
+  <div
+    class="roulette__spinner"
+    :style="`background-image: url('${require('@/assets/img/pages/frame.svg')}')`"
+  >
+    <div class="roulette__spinner__items-list">
+      <div
+        v-for="n in 10"
+        :key="n"
+        class="roulette__spinner__items-list__item"
+        style="border-color: #FF0000; box-shadow: inset 0px 2px 50px 0 rgba(255, 0, 0, 0.25)"
+      >
+        <img class="item-img" src="@/assets/img/examples/AK-Vulcan.png">
+        <span class="item-type">AKR</span>
+        <span class="item-name">Treasure Hunter</span>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style lang="less" scoped>
+@import '~assets/less/utils/_vars';
+@import '~assets/less/utils/_mixins';
+
+.roulette {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  &__case-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40px;
+    margin-bottom: 8px;
+    span {
+      font-family: Roboto Condensed;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 24px;
+      text-transform: uppercase;
+      color: #fff;
+      margin: 0 22px;
+    }
+    .circles {
+      display: flex;
+      align-items: center;
+      &:first-child {
+        .circle:first-child {
+          .middle-title-diamond();
+        }
+        .circle:last-child {
+          .big-title-diamond();
+          margin-left: 14px;
+        }
+      }
+      &:last-child {
+        .circle:first-child {
+          .big-title-diamond();
+        }
+        .circle:last-child {
+          .middle-title-diamond();
+          margin-left: 14px;
+        }
+      }
+    }
+  }
+  &__case-type {
+    font-family: Roboto Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    text-transform: uppercase;
+    color: #C5C5C5;
+  }
+  &__spinner {
+    width: 1028px;
+    height: 144px;
+    margin-top: 103px;
+    margin-bottom: 45px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    overflow: hidden;
+    &__items-list {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: nowrap;
+      height: 100%;
+      padding-left: 190px;
+      clip-path: polygon(46px 72px, 112px 0, 917px 0, 982px 72px, 917px 144px, 112px 144px);
+      &__item {
+        z-index: -1;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        box-sizing: border-box;
+        border: 2px solid #fff;
+        height: 120px;
+        width: 210px;
+        margin-left: 3px;
+        .item-img {
+          width: 185px;
+          transform: rotate(15deg);
+          z-index: -1;
+        }
+        .item-type {
+          position: absolute;
+          left: 7px;
+          bottom: 23px;
+          font-family: Roboto Condensed;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 16px;
+          color: #fff;
+        }
+        .item-name {
+          position: absolute;
+          left: 7px;
+          bottom: 6px;
+          font-family: Roboto Condensed;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 14px;
+          color: rgba(255, 255, 255, 0.7);
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/case.vue
+++ pages/case.vue
@@ -1,7 +1,72 @@
 <template>
 <div>
 
+  <Roulette v-if="spin"></Roulette>
 
+  <!-- <div class="open-case" v-if="!spin">
+    <div class="open-case__title">
+      <div class="circles">
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+      <span>Ножевой</span>
+      <div class="circles">
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+    </div>
+    <span class="open-case__type">Кейс</span>
+    <div
+      class="open-case__img"
+      :style="`background-image: url('${require('@/assets/img/cases/knife-case.png')}')`"
+    >
+      <img class="open-case__img__arrow arrow-left" src="@/assets/img/pages/arrows.svg">
+      <img class="open-case__img__arrow arrow-right" src="@/assets/img/pages/arrows.svg">
+    </div>
+    <button
+      class="open-case__btn"
+      @click="roll"
+    >
+      Открыть кейс за <span>500₽</span>
+    </button>
+    <div class="open-case__times">
+      <ul>
+        <li class="active">x1</li>
+        <li>x2</li>
+        <li>x3</li>
+        <li>x4</li>
+        <li>x5</li>
+        <li>x10</li>
+      </ul>
+    </div>
+  </div> -->
+
+  <div class="case-opened" v-if="!spin">
+    <div class="case-opened__title">
+      <div class="circles">
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+      <span>Treasure Hunter</span>
+      <div class="circles">
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+    </div>
+    <span class="case-opened__type">AKR</span>
+    <div
+      class="case-opened__item"
+      :style="`background-image: url('${require('@/assets/img/pages/skindrop-sprites.png')}')`"
+      style="background-position: -3120px -100px"
+    >
+      <img class="case-opened__item__img" src="@/assets/img/examples/AK-Vulcan.png">
+    </div>
+    <div class="case-opened__buttons">
+      <button class="case-opened__buttons--grey">Попробовать ещё</button>
+      <button class="case-opened__buttons--purple">Продать за <span>10.000₽</span></button>
+      <button class="case-opened__buttons--grey">На главную</button>
+    </div>
+  </div>
 
   <div class="case-items">
      <div class="case-items__title">
@@ -20,7 +85,7 @@
         v-for="n in 21"
         :key="n"
         class="case-item"
-        :style="`background-image: url('${require('@/assets/img/skins/skindes-sprites.png')}')`"
+        :style="`background-image: url('${require('@/assets/img/pages/skindes-sprites.png')}')`"
         style="background-position: -900px 45%"
       >
         <img class="case-item__img" src="@/assets/img/examples/AK-Vulcan.png">
@@ -34,9 +99,20 @@
 </template>
 
 <script>
+import Roulette from '@/components/case-opening/Roulette'
+
 export default {
+  data: () => ({
+    spin: false
+  }),
   components: {
-    //
+    Roulette
+  },
+  methods: {
+    roll () {
+      this.spin = true
+      // roulette.spin()
+    }
   }
 }
 </script>
@@ -44,6 +120,328 @@ export default {
 <style lang="less" scoped>
 @import '~assets/less/utils/_vars';
 @import '~assets/less/utils/_mixins';
+
+.open-case {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  &__title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40px;
+    margin-bottom: 8px;
+    span {
+      font-family: Roboto Condensed;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 24px;
+      text-transform: uppercase;
+      color: #fff;
+      margin: 0 22px;
+    }
+    .circles {
+      display: flex;
+      align-items: center;
+      &:first-child {
+        .circle:first-child {
+          .middle-title-diamond();
+        }
+        .circle:last-child {
+          .big-title-diamond();
+          margin-left: 14px;
+        }
+      }
+      &:last-child {
+        .circle:first-child {
+          .big-title-diamond();
+        }
+        .circle:last-child {
+          .middle-title-diamond();
+          margin-left: 14px;
+        }
+      }
+    }
+  }
+  &__type {
+    font-family: Roboto Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    text-transform: uppercase;
+    color: #C5C5C5;
+  }
+  &__img {
+    height: 310px;
+    width: 400px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    margin-top: 25px;
+    position: relative;
+    &__arrow {
+      position: absolute;
+      top: 28%;
+    }
+    .arrow-left {
+      left: -79%;
+      @media screen  and (max-width: 1500px) {
+        left: -55%;
+      }
+    }
+    .arrow-right {
+      right: -79%;
+      transform: rotate(180deg);
+      @media screen  and (max-width: 1500px) {
+        right: -55%;
+      }
+    }
+  }
+  &__btn {
+    position: relative;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    width: 295px;
+    height: 65px;
+    background-color: @defPurpleBg;
+    border-bottom: 4px solid @defPurpleBorder;
+    font-family: Roboto Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    color: #FFFFFF;
+    margin-top: 33px;
+    span {
+      font-weight: bold;
+      color: @defFontYellow;
+    }
+    &:before, &:after {
+      z-index: -1;
+      box-sizing: border-box;
+      position: absolute;
+      content: '';
+      width: 45.5px;
+      height: 45.5px;
+      background-color: @defPurpleBg;
+      border-bottom: 4px solid @defPurpleBorder;
+    }
+    &:before {
+      top: 9px;
+      left: -23px;
+      transform: rotate(45deg);
+    }
+    &:after {
+      top: 9px;
+      right: -23px;
+      transform: rotate(-45deg);
+    }
+    &:hover {
+      background-color: @defPurpleBgHover;
+      &:before, &:after { background-color: @defPurpleBgHover; }
+    }
+  }
+  &__times {
+    height: 38px;
+    width: 295px;
+    border-top: 1px solid @defPurpleBorder;
+    border-bottom: 1px solid @defPurpleBorder;
+    margin-top: 22px;
+    ul {
+      height: 100%;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      li {
+        height: 100%;
+        box-sizing: border-box;
+        cursor: pointer;
+        font-family: Roboto Condensed;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 38px;
+        color: #fff;
+        padding: 0 15px;
+        border-left: 1px solid @defPurpleBorder;
+        &:first-child {
+          border-left: none;
+          position: relative;
+          &:before {
+            box-sizing: border-box;
+            z-index: -1;
+            content: '';
+            position: absolute;
+            width: 25px;
+            height: 25px;
+            top: 4.5px;
+            left: -13px;
+            border-left: 1px solid @defPurpleBorder;
+            border-bottom: 1px solid @defPurpleBorder;
+            transform: rotate(45deg);
+          }
+        }
+        &:last-child {
+          position: relative;
+          &:after {
+            box-sizing: border-box;
+            z-index: -1;
+            content: '';
+            position: absolute;
+            width: 26px;
+            height: 26px;
+            top: 4.5px;
+            right: -13px;
+            border-right: 1px solid @defPurpleBorder;
+            border-bottom: 1px solid @defPurpleBorder;
+            transform: rotate(-45deg);
+          }
+        }
+        &:hover {
+          background-color: rgba(109, 28, 212, 0.33);
+        }
+      }
+      li.active {
+        box-sizing: border-box;
+        background-color: @defPurpleBg;
+        border-bottom: 3px solid @defPurpleBorder;
+        &:before, &:after {
+          background-color: @defPurpleBg;
+          border-bottom: 3px solid @defPurpleBorder;
+        }
+      }
+    }
+  }
+}
+
+.case-opened {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 34px;
+  &__title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40px;
+    margin-bottom: 8px;
+    span {
+      font-family: Roboto Condensed;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 24px;
+      text-transform: uppercase;
+      color: #fff;
+      margin: 0 22px;
+    }
+    .circles {
+      display: flex;
+      align-items: center;
+      &:first-child {
+        .circle:first-child {
+          .middle-title-diamond();
+        }
+        .circle:last-child {
+          .big-title-diamond();
+          margin-left: 14px;
+        }
+      }
+      &:last-child {
+        .circle:first-child {
+          .big-title-diamond();
+        }
+        .circle:last-child {
+          .middle-title-diamond();
+          margin-left: 14px;
+        }
+      }
+    }
+  }
+  &__type {
+    font-family: Roboto Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    text-transform: uppercase;
+    color: #C5C5C5;
+  }
+  &__item {
+    background-repeat: no-repeat;
+    background-size: auto 520px;
+    &__img {
+      width: 530px;
+      margin-top: 125px;
+    }
+  }
+  &__buttons {
+    margin-top: 66px;
+    width: 882px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &--purple, &--grey {
+      position: relative;
+      cursor: pointer;
+      outline: none;
+      border: none;
+      width: 240px;
+      height: 65px;
+      background-color: @defPurpleBg;
+      border-bottom: 4px solid @defPurpleBorder;
+      font-family: Roboto Condensed;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 24px;
+      color: #FFFFFF;
+      span {
+        font-weight: bold;
+        color: @defFontYellow;
+      }
+      &:before, &:after {
+        z-index: -1;
+        box-sizing: border-box;
+        position: absolute;
+        content: '';
+        width: 45.5px;
+        height: 45.5px;
+        background-color: @defPurpleBg;
+        border-bottom: 4px solid @defPurpleBorder;
+      }
+      &:before {
+        top: 9px;
+        left: -23px;
+        transform: rotate(45deg);
+      }
+      &:after {
+        top: 9px;
+        right: -23px;
+        transform: rotate(-45deg);
+      }
+      &:hover {
+        background-color: @defPurpleBgHover;
+        &:before, &:after { background-color: @defPurpleBgHover; }
+      }
+    }
+    &--grey {
+      width: 210px;
+      background-color: @defGreyBorder;
+      border-color: #222122;
+      &:before, &:after {
+        background-color: @defGreyBorder;
+        border-color: #222122;
+      }
+      &:hover {
+        background-color: @defGreyBgHover;
+        &:before, &:after { background-color: @defGreyBgHover }
+      }
+    }
+  }
+}
 
 // styles for .case-item in main.less
 
@@ -57,8 +455,8 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: 40px;
-    margin-bottom: 20px;
+    margin-top: 52px;
+    margin-bottom: 5px;
     span {
       font-family: Roboto Condensed;
       font-style: normal;
